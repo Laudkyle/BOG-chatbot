1e7bb276d3a776cc73972ee79ea901e8c22e3422
laudkyle <kyleaby1@gmail.com>
2024-03-20 15:07:22 -0700

    chat bot done

--- chat.py
+++ chat.py
@@ -34,14 +34,20 @@ while True:
     sentence =tokenize(sentence)
     X =bag_of_words(sentence, all_words)
     X.reshape(1,X.shape[0])
-    X = X.torch.from_numpy()
+    X = X.torch.from_numpy(X)
     output = model(X)
 
     _,pred = torch.max(output, dim=1)
     tag = tags[pred.item()]
 
-    for intent in intents["intents"]:
-        if tag ==  intent["tag"]:
-            print(f"{bot_name} : { random.choice(intent['responses'])}")
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][pred.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag ==  intent["tag"]:
+                print(f"{bot_name} : { random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name} : I do not understand ....")
 
 
